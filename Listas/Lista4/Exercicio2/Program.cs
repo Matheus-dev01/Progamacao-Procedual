3e7db51862f638c5af1109a2c3db2ac528bfe7e9
Matheus-dev01 <matheuspt169@gmail.com>
2024-03-13 21:31:06 -0300

    Lista 4 - Laços de repetição aninhados

---
+++ Listas/Lista4/Exercicio2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        int contadorLoop = 1;
+
+        while (contadorLoop == 1)
+        {
+        System.Console.WriteLine("Digite a altura do triangulo: ");
+        int AlturaTriangulo = int.Parse(Console.ReadLine());
+
+        if (AlturaTriangulo >= 1 && AlturaTriangulo <= 9)
+        {
+            for (int coluna = 1; coluna <= AlturaTriangulo; coluna++) 
+            {
+                for (int contador = 1; contador <= coluna; contador++)
+                {
+                    Console.Write(contador);
+                }
+                System.Console.WriteLine();
+            }
+            contadorLoop++;
+        }
+        else
+        {
+            System.Console.WriteLine("Altura invalida, insira uma altura entre 1 e 9");
+        }
+        }
+    }
+}
