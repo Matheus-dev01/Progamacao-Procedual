f7e37f104a5f7b60beb67b9f70727dbaa3367e48
Matheus-dev01 <matheuspt169@gmail.com>
2024-03-16 11:30:41 -0300

    Laços de repetição até o burnout - Parte 1

---
+++ Aulas/Aula9/ex03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+	{
+		static void Main(string[] args)
+		{
+      int somatorio = 0;
+
+      int numeroDigitado = int.Parse(Console.ReadLine());
+
+      if (numeroDigitado > 0)
+      {
+        for (int contador = 0; contador < numeroDigitado; contador++)
+        {
+          int sobra = contador % 2;
+          if (sobra != 0)
+          {
+            somatorio = somatorio + contador;
+          }
+        }
+        System.Console.WriteLine($"O somatorio de todos numeros impares eh: {somatorio}");
+      }
+      else if (numeroDigitado < 0)
+      {
+        for (int contador = 0; contador >= numeroDigitado; contador--)
+        {
+          int sobra = contador % 2;
+          if (sobra == 0)
+          {
+            System.Console.WriteLine(contador);
+          }        
+        }
+      }
+      else 
+      {
+        System.Console.WriteLine("nada a fazer aqui");
+      }
+		}
+	}
