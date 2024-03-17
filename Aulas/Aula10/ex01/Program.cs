f049ec6e0c4b1aa8731f902179e8de3942464211
Matheus-dev01 <matheuspt169@gmail.com>
2024-03-17 12:05:33 -0300

    Laços de repetição até o Burnout - Parte 2

---
+++ Aulas/Aula10/ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Porgram
+{
+    static void Main (string[] args)
+    {
+        int contador = 0;
+
+        while (contador <= 99)
+        {
+            System.Console.WriteLine(contador);
+            contador = contador + 3;
+        }
+    }
+}
---
+++ Aulas/Aula10/ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        for (int linha = 0; linha <= 9; linha++)
+        {
+            for (int coluna = 1; coluna <= linha; coluna++)
+            {
+                System.Console.Write(coluna);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
