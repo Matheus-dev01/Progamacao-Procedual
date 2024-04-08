59b521719f71d06e62f9aa8c9bdfcdacc6d076ea
Matheus-dev01 <matheuspt169@gmail.com>
2024-04-08 13:52:22 -0300

    Lista 8 - struct

---
+++ Listas/Lista8/exercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza[] Pizza = new Pizza[1];
+
+        for (int contador = 0; contador < Pizza.GetLength(0); contador++)
+        {
+            Pizza[contador].id = Convert.ToInt32(Console.ReadLine());
+            Pizza[contador].sabor = Console.ReadLine();
+            Pizza[contador].preco = Convert.ToDouble(Console.ReadLine());
+        }
+        for (int contador = 0; contador < Pizza.GetLength(0); contador++)
+        {
+            System.Console.WriteLine(Pizza[contador].id);
+            System.Console.WriteLine(Pizza[contador].sabor);
+            System.Console.WriteLine(Pizza[contador].preco); 
+        }
+    }
+}
---
+++ Listas/Lista8/exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int QUANTIDADE_DE_PRODUTOS = 4;
+        Produto[] produtos = new Produto[QUANTIDADE_DE_PRODUTOS];
+
+        for (int contador = 0; contador < produtos.GetLength(0); contador++)
+        {
+         produtos[contador].id = Convert.ToInt32(Console.ReadLine());
+         produtos[contador].nome = Console.ReadLine();
+         produtos[contador].preco = Convert.ToDouble(Console.ReadLine());
+         produtos[contador].disponivelEmEstoque = Convert.ToBoolean(Console.ReadLine());
+
+        }
+        for (int contador = 0; contador < produtos.GetLength(0); contador++)
+        {
+            if(produtos[contador].disponivelEmEstoque == true)
+            {
+                System.Console.WriteLine(produtos[contador].nome);
+                System.Console.WriteLine(Math.Round(produtos[contador].preco, 2));
+            }
+        }
+    }
+}
